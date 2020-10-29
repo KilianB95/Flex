718f482288b71829f3abef9752a0baca47450b7d
Kilian <31303@ma-web.nl>
2020-10-29 14:17:17 +0100

    PILimage file updated

--- 03-MemeGifs/PILimage.py
+++ 03-MemeGifs/PILimage.py
@@ -20,3 +20,19 @@ print("De afbeelding is" + str(breedte) + "pixels breed en" + str(hoogte) + "pix
 # Andere info uitlezen en tonen
 print(afbeelding.format, afbeelding.size, afbeelding.mode)
 
+# breedte en hoogte door twee delen (en afronden naar beneden)
+
+helft_breedte = afbeelding.width//2
+helft_hoogte = afbeelding.height//2
+
+# Met het resize commando kun je de afmetingen van afbeelding aanpassen
+# Deze verwacht een Tuple variabele:
+nieuwe_afmeting = (helft_breedte, helft_hoogte)
+
+# Hier wordt de afbeelding kleiner gemaakt en opgeslagen in een nieuwe variabele!
+kleinere_afbeelding = afbeelding.resize(nieuwe_afmeting)
+
+# Nu de kleinere afbeelding opslaan me save(). Gebruik de originele bestandsnaam met ergens "klein" er in.
+kleinere_afbeelding.save("anime.jpg")
+
+# zet hier nu zelf de python code onder de kleinere afbeelding op het scherm te tonen
